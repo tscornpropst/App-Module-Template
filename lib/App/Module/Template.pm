5ffc6145ed2ad3be0be64f75d3f11a566d7f6bfd
tscornpropst <tscornpropst@gmail.com>
2014-08-30 23:08:49 -0400

    version bump. Update Changes.

--- lib/App/Module/Template.pm
+++ lib/App/Module/Template.pm
@@ -5,7 +5,7 @@ use 5.016;
 use strict;
 use warnings;
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 use base qw(Exporter);
 
--- lib/App/Module/Template/Initialize.pm
+++ lib/App/Module/Template/Initialize.pm
@@ -3,7 +3,7 @@ package App::Module::Template::Initialize;
 use strict;
 use warnings;
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 use base qw(Exporter);
 
